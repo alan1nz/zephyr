46344fb94e4bf0b0998bd8c19804e44028dff252
peng <pencao111@gmail.com>
2026-08-04 10:34:19 +0000

    Add svd and zed task

--- stepper_project/app/src/main.c
+++ stepper_project/app/src/main.c
@@ -1,12 +1,30 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+
+// #define LED DT_ALIAS(led0)
+// static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED, gpios);
 
 int main()
 {
+	// if (!gpio_is_ready_dt(&led))
+	// {
+	// 	printk("LED is not ready");
+	// 	return 1;
+	// }
+
+	// if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) != 0)
+	// {
+	// 	printk("LED failed to configure");
+	// 	return 1;
+	// }
+
 	while (1)
 	{
-
 		printk("hello there gay boi\r\n");
-		k_msleep(1);
+		// gpio_pin_toggle_dt(&led);
+		k_msleep(5000);
 	}
 }
